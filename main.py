3fe311b11dd7c04be98db20249509fb6305b670b
SiriusMklain <sergeimakarenko5991@gmail.com>
2023-02-02 03:47:40 +0300

    edit token

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import openai
 import telebot
 from dotenv import load_dotenv
+load_dotenv()
 
 openai.api_key = os.environ.get("TOKEN_GPT")
 bot = telebot.TeleBot(os.environ.get("TOKEN_TELEGRAM"))
